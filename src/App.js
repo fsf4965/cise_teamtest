744a35d39c904d0d1b26da10301d55f82ead46b1
aatxegorri <a.gilbert416@gmail.com>
2021-04-23 18:30:08 +1200

    npm initial setup

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          rats, we're rats
+        </p>
+	<p>
+          we're the rats
+        </p>
+	<p>
+          we stalk at night, we prey at night
+        </p>
+	<p>
+          we're the rats
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
