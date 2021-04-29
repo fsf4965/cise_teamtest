e6a5d9a2d143eafd03421571c35872c3345bea36
zapier2 <gerardalexigomez@yahoo.com>
2021-04-30 11:51:37 +1200

    added css to buttons

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import '../App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 class Search extends Component {
-    render() {
-        return (
-            <div className="Homepage">
-                <div className="container">
-                    <br/>
-                    <h2 className="header">Search</h2>
-
-                    <Link to="/"><button type="button">
-                    Homepage
-                    </button> </Link>
-                    <br />
-                    <br />
-                    <hr />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="Homepage">
+        <div className="container">
+          <br />
+          <h2 className="header">Search</h2>
+          <div className="buttons">
+            <Link to="/">
+              <button class="btn homepage" type="button">
+                Homepage
+              </button>{" "}
+            </Link>
+          </div>
+          <br />
+          <br />
+          <hr />
+        </div>
+      </div>
+    );
   }
+}
 export default Search;
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import '../App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 class homepage extends Component {
-    render() {
-        return (
-            <div className="Homepage">
-                <div className="container">
-                    <br/>
-                    <h2 className="header">SEEDS</h2>
-
-                    <Link to="/Search"><button type="button">
-                    Search
-                    </button> </Link>
-                    <br />
-                    <br />
-                    <hr />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="Homepage">
+        <div className="container">
+          <br />
+          <h2 className="header">SEEDS</h2>
+          <div className="buttons">
+            <Link to="/Search">
+              <button class="btn search" type="button">
+                Search
+              </button>{" "}
+            </Link>
+          </div>
+          <br />
+          <br />
+          <hr />
+        </div>
+      </div>
+    );
   }
+}
 export default homepage;
