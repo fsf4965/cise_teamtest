c370452b051847d5f35e43c31a350308b11c54e6
aatxegorri <a.gilbert416@gmail.com>
2021-05-04 16:53:25 +1200

    app.js imports changed to .js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-import home from './components/home';
-import search from './components/search';
+import home from './components/home.js';
+import search from './components/search.js';
 
 class App extends Component {
   render() {
