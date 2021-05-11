e0905b9fb681c6333b0724fce4c93e2a91422fc6
fengv1976 <fengv1976@gmail.com>
2021-05-11 21:08:57 +1200

    changes to backend app.js

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const connectDB = require('./config/db');
+var cors = require('cors');
 
+//
+/* This code have issue
+const entry = require('./routes/api/entries');
+*/
 const app = express();
 
 // Connect Database
 connectDB();
 
+app.use(express.json({ extended: false }));
 
+app.use(cors({ origin: true, credentials: true }));
 
 app.get('/', (req, res) => res.send('Hello world!'));
+
+app.use('/api/entries', entry);
 
 const port = process.env.PORT || 8082;
 
