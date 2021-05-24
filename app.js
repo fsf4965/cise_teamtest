e2ad8e9f73fb4895044dd1741680519a4bd55333
fengv1976 <fengv1976@gmail.com>
2021-05-24 12:19:03 +1200

    f

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // app.js
-
+const path = require('path'); 
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
+require("dotenv").config({path: "./config.env"});
 
 // routes
 const books = require('./routes/api/books');
@@ -18,11 +19,21 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
-
 // use Routes
 app.use('/api/books', books);
 
-const port = process.env.PORT || 8082;
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/search-app/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname,"search-app","build","index.html"));
+    });
+} else{
+    app.get("/", (req,res) =>{
+        res.send("api running");
+    });
+}
+
+const port = process.env.PORT;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
