810f7c31c2dd97ce91a78cc1a96b513e665eaf26
fsf4965 <fsf4965@autuni.ac.nz>
2021-05-26 18:33:47 +1200

    Update books.js

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,39 +1,62 @@
-// app.js
-const path = require('path'); 
-const express = require('express');
-const connectDB = require('./config/db');
-var cors = require('cors');
-require("dotenv").config({path: "./config.env"});
+    // routes/api/books.js
 
-// routes
-const books = require('./routes/api/books');
-
-const app = express();
-
-// Connect Database
-connectDB();
-
-// cors
-app.use(cors({ origin: true, credentials: true }));
-
-// Init Middleware
-app.use(express.json({ extended: false }));
-
-// use Routes
-app.use('/api/books', books);
-
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/client-app/build")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname,"client-app","build","index.html"));
+    const express = require('express');
+    const router = express.Router();
+    
+    // Load Book model
+    const Book = require('../../models/Book');
+    //const Article = require('../../models/Article');
+    
+    // @route GET api/books/test
+    // @description tests books route
+    // @access Public
+    router.get('/test', (req, res) => res.send('book route testing!'));
+    
+    // @route GET api/books
+    // @description Get all books
+    // @access Public
+    router.get('/', (req, res) => {
+      Book.find()
+        .then(books => res.json(books))
+        .catch(err => res.status(404).json({ nobooksfound: 'No Books found' }));
     });
-} else{
-    app.get("/", (req,res) =>{
-        res.send("api running");
+    
+    // @route GET api/books/:id
+    // @description Get single book by id
+    // @access Public
+    router.get('/:id', (req, res) => {
+      Book.findById(req.params.id)
+        .then(book => res.json(book))
+        .catch(err => res.status(404).json({ nobookfound: 'No Book found' }));
     });
-}
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+    
+    // @route GET api/books
+    // @description add/save book
+    // @access Public
+    router.post('/', (req, res) => {
+      Book.create(req.body)
+        .then(book => res.json({ msg: 'Book added successfully' }))
+        .catch(err => res.status(400).json({ error: 'Unable to add this book' }));
+    });
+    
+    // @route GET api/books/:id
+    // @description Update book
+    // @access Public
+    router.put('/:id', (req, res) => {
+      Book.findByIdAndUpdate(req.params.id, req.body)
+        .then(book => res.json({ msg: 'Updated successfully' }))
+        .catch(err =>
+          res.status(400).json({ error: 'Unable to update the Database' })
+        );
+    });
+    
+    // @route GET api/books/:id
+    // @description Delete book by id
+    // @access Public
+    router.delete('/:id', (req, res) => {
+      Book.findByIdAndRemove(req.params.id, req.body)
+        .then(book => res.json({ mgs: 'Book entry deleted successfully' }))
+        .catch(err => res.status(404).json({ error: 'No such a book' }));
+    });
+    
+    module.exports = router;
